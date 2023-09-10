c2452429059df9a91af51eceddb979c3e9ea7db2
Wagner Prado <45642338+wapcce@users.noreply.github.com>
2023-09-10 09:00:05 -0300

    Create EquilibrandoSaldo.java
    Desafio Java DIO Santander Equilibrando o Saldo

---
+++ EquilibrandoSaldo.java
@@ -0,0 +1,54 @@
+/*
+Descrição
+Para esse desafio, considere que você foi contratado por uma empresa bancária para auxiliar nas implementações e melhorias do sistema empresarial. Em uma análise inicial, foi identificado pela equipe financeira a necessidade de desenvolver uma solução que permita ao cliente equilibrar seu saldo bancário. Dessa forma, o programa deve solicitar uma entrada que representa o saldo atual do funcionário, e após, seja informado o valor de duas transações, sendo elas: um depósito e um saque. O programa deve atualizar o saldo com base nas transações e exibir o saldo final.
+
+Informação: As transações de depósito e retirada devem ser tratadas como valores positivos e negativos, respectivamente, para garantir que o cálculo do saldo final seja realizado corretamente.
+ 
+
+Entrada
+saldoAtual: um número decimal representando o saldo atual da conta bancária.
+valorDeposito: um número decimal representando o valor a ser depositado na conta.
+valorRetirada: um número decimal representando o valor a ser retirado da conta.
+
+Regra de Formatação: Considere apenas uma casa decimal para esse desafio.
+
+Saída
+ Um número decimal que representa o saldo atualizado na conta bancária após o processamento das transações.
+
+Exemplos
+A tabela abaixo apresenta exemplos com alguns dados de entrada e suas respectivas saídas esperadas. Certifique-se de testar seu programa com esses exemplos e com outros casos possíveis.
+
+Entrada	Saída
+1000
+500
+200	Saldo atualizado na conta: 1300.0
+100
+10
+50	Saldo atualizado na conta: 60.0
+4000
+1500
+200	Saldo atualizado na conta: 5300.0
+*/
+
+
+import java.util.Scanner;
+
+public class EquilibrandoSaldo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //System.out.println("Favor informar o saldo")
+        double saldoAtual = scanner.nextDouble();
+        //System.out.println("Favor informar o valor do déposito")
+        double valorDeposito = scanner.nextDouble();
+        //System.out.println("Favor informar o valor que deseja retirar")
+        double valorRetirada = scanner.nextDouble();
+
+        double saldo = (saldoAtual+valorDeposito)- valorRetirada;
+        
+        System.out.printf("Saldo atualizado na conta: %.1f",saldo);
+       //TODO: Calcular o saldo atualizado de acordo com a descrição deste desafio.
+
+     //TODO: Imprimir o a saída de conforme a tabela de exemplos (uma casa decimal).
+    }
+}
